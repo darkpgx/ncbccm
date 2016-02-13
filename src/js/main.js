cd064d727b27172b62ba95da3caa2ad55eaadce5
Song Zheng <song@tokbox.com>
2016-02-13 12:10:47 -0800

    working react router - new version

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,23 @@
+/*
 import {ResourceEdit} from '../react/resources/Edit.js';
 import {ResourceIndex} from '../react/resources/Index.js';
+*/
 import {IndexPage} from '../react/IndexPage.js';
-var Router = require('react-router');
-var React = require('react');
+import { Router, Route, Link, hashHistory } from 'react-router';
+import { render } from 'react-dom';
+import React from 'react';
 
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
-var RouteHandler = Router.RouteHandler;
-
-var App = React.createClass({
-  render: function () {
-    return (<RouteHandler/>);
+const App = React.createClass({
+  render() {
+    return (
+      <div> 
+        <h1> hello world </h1>
+        {this.props.children}
+      </div>
+    )
   }
 });
 
-var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="resources" handler={App}>
-      <Route path=":id" handler={ResourceEdit}/>
-      <Route path=":id/edit" handler={ResourceEdit}/>
-      <DefaultRoute name="resourceIndex" handler={ResourceIndex}/>
-    </Route>
-    <DefaultRoute name="index" handler={IndexPage}/>
-  </Route>
-);
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.getElementById('reactContainer'));
-});
-
+render((
+<App />
+), document.getElementById('reactContainer'))
