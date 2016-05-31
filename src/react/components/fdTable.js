15900d007567ca209ca9c7671a6a544c83fca16d
Larry Piao <darkpgx@gmail.com>
2016-05-30 20:39:12 -0700

    memorial, 2016 week update

--- src/react/components/fdTable.js
+++ src/react/components/fdTable.js
@@ -31,13 +31,13 @@ export class FDTable extends React.Component {
 
     this.state = {
       myTableData: [
-        {date: 'May 23, 2016', verses: '2 Chronicles 7:14', verses2: 'Psalm 135:1-3', verses3: 'Romans 4:18-25'},
-        {date: 'May 24, 2016', verses: 'Romans 5:6-8', verses2: 'Romans 5:9-11', verses3: 'Psalm 136:1-9'},
-        {date: 'May 25, 2016', verses: '1 Kings 11:9-11', verses2: 'Romans 6:11-14', verses3: 'Romans 6:15-23'},
-        {date: 'May 26, 2016', verses: 'Proverbs 1:7-9', verses2: 'Proverbs 1:10-16', verses3: 'Proverbs 3:1-12'},
-        {date: 'May 27, 2016', verses: 'Proverbs 6:16-19', verses2: 'Proverbs 6:20-24', verses3: 'Romans 8:1-9'},
-        {date: 'May 28, 2016', verses: 'Proverbs 9:9-10', verses2: 'Proverbs 9:11-12', verses3: 'Romans 9:14-21'},
-        {date: 'May 29, 2016', verses: 'Romans 10:8-9', verses2: 'Romans 10:10-13', verses3: 'Proverbs 12:1-13'}
+        {date: 'May 30, 2016', verses: 'Romans 11:36', verses2: 'Proverbs 15:1-5', verses3: 'Proverbs 15:6-16'},
+        {date: 'May 31, 2016', verses: 'Romans 12:2', verses2: 'Proverbs 18:1-5', verses3: 'Proverbs 18:6-24'},
+        {date: 'June 01, 2016', verses: 'Romans 13:8-9', verses2: 'Proverbs 19:1-5', verses3: 'Proverbs 19:6-29'},
+        {date: 'June 02, 2016', verses: 'Romans 14:10-12', verses2: 'Proverbs 22:1-5', verses3: 'Proverbs 22:6-29'},
+        {date: 'June 03, 2016', verses: 'Proverbs 25:21-22', verses2: 'Proverbs 27:1-2', verses3: 'Romans 15:1-7'},
+        {date: 'June 04, 2016', verses: 'Psalm 60:12', verses2: 'Proverbs 29:23-27', verses3: 'Romans 16:17-20'},
+        {date: 'June 05, 2016', verses: 'Proverbs 30:5', verses2: 'Psalm 33:18-22', verses3: 'Ephesians 1:1-14'}
       ],
     };
   }
